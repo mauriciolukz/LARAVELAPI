3e59328cccd84a271e9ccb5ddff558d09dfe39b0
Mauricio Palacios <mauriciopalacio1990@gmail.com>
2020-05-31 09:06:16 -0600

    documentando api con swagger

--- app/Http/Controllers/MC40200Controller.php
+++ app/Http/Controllers/MC40200Controller.php
@@ -5,42 +5,38 @@ namespace App\Http\Controllers;
 use App\MC40200;
 use Illuminate\Http\Request;
 
-
-/**
-* @OA\Info(title="API Moneda", version="1.0")
-*
-* @OA\Server(url="http://localhost:8000")
-*/
 class MC40200Controller extends Controller
 {
     /**
     * @OA\Get(
-    *     path="/api/MC40200/getCurrencyById/",
-    *     summary="Mostrar la moneda buscada.",
-    *     @OA\Parameter(
-    *         name="id",
-    *         in="query",
-    *         type="string",
-    *         description="Your divisa",
-    *         required=true,
-    *     ),
+    *     path="/MC40200/getCurrencyById/{id}",
+    *     tags={"API PARA MONEDA"},
+    *     summary="Mostrar la moneda buscada por id.",
+    *       description="Returns project data",
+    *       @OA\Parameter(
+    *          name="id",
+    *          description="Divisa Id",
+    *          required=true,
+    *          in="path",
+    *          @OA\Schema(
+    *              type="string"
+    *          )
+    *      ),
     *     @OA\Response(
     *         response=200,
-    *         description="Mostrar la moneda."
+    *         description="Successful operation",
     *     ),
     *     @OA\Response(
     *         response="default",
     *         description="Ha ocurrido un error."
-    *     )
+    *     ),
+    *     @OA\Response(
+    *          response=401,
+    *          description="Unauthenticated",
+    *      ),
     * )
     */
 
-    /**
-     * Combine firstname and lastname
-     *
-     * @param $id
-     * 
-     */
     public function getCurrencyById(Request $request,$id){
         //$MC40200 = \DB::select(\DB::raw("exec DYNAMICS.dbo.zDP_MC40200SS_1 '".$id."'"));
         $MC40200 = MC40200::where('CURNCYID', $id)->first();
--- app/MC40200.php
+++ app/MC40200.php
@@ -4,10 +4,57 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     title="MC40200",
+ *     description="Tabla para divisas",
+ *     @OA\Xml(
+ *         name="MC40200"
+ *     )
+ * )
+ */
 class MC40200 extends Model
 {
     //
     protected $table = 'MC40200';
 
     protected $primaryKey = 'DEX_ROW_ID';
+
+    public $CURNCYID;
+
+    /**
+     * @OA\Property(
+     *      title="CURNCYID",
+     *      description="Id de moneda",
+     *      example="This is new project's description"
+     * )
+     *
+     * @var integer
+     */
+
+    public $CURRNIDX;
+
+    /**
+     * @OA\Property(
+     *      title="CURRNIDX",
+     *      description="indice de moneda",
+     *      example="This is new project's description",
+     *      format="int64",
+     *      example=100
+     * )
+     *
+     * @var integer
+     */
+
+
+     /**
+     * @OA\Property(
+     *     title="SY01400",
+     *     description="Project author's user model"
+     * )
+     *
+     * @var \App\Models\SY01400
+     */
+    private $SY01400;
+    
 }
