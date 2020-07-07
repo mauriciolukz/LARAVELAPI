d4f57568874cff37f50e81a8ca9fe8f844c5b908
Mauricio Palacios <mauriciopalacio1990@gmail.com>
2020-07-06 23:31:50 -0600

    cam

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'DYNAMICS'),
+    'default' => env('DB_CONNECTION', 'DUDE'),
 
     /*
     |--------------------------------------------------------------------------
