6360878b40011490a790796f5a56b7c650442dc4
Mauricio Palacios <mauriciopalacio1990@gmail.com>
2020-07-14 12:08:55 -0600

    doble header error

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
         // return $next($request);
         return $next($request)
         ->header("Access-Control-Allow-Origin", "*")
-        ->headerheader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization")
+        ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization")
         ->header("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
     }
 }
