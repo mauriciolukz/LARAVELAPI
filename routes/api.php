e6fd1bd6de77733439daa1d225ed7a47c1fe1d05
Mauricio Palacios <mauriciopalacio1990@gmail.com>
2020-07-15 08:59:02 -0600

    borrando endpoint para test

--- routes/api.php
+++ routes/api.php
@@ -37,8 +37,7 @@ Route::group(['middleware' => 'auth.jwt'], function () {
 
     //Genericos
     Route::get('generic/getNextNoteIndex/', 'GeneriController@getNextNoteIndex');
-    Route::get('generic/getHola/', 'GeneriController@getHola');
-    Route::get('common/getHola/', 'CommonController@getHola');
+
 }); 
 
 
