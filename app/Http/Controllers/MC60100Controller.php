d300e32b4869994c3688fd491281c916e8509c7f
Mauricio Palacios <mauriciopalacio1990@gmail.com>
2020-09-19 10:18:16 -0600

    Agregando modelo y controlador para acceso de multimoneda

---
+++ app/Http/Controllers/MC60100Controller.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MC60100;
+use Illuminate\Http\Request;
+
+class MC60100Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MC60100  $mC60100
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MC60100 $mC60100)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MC60100  $mC60100
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MC60100 $mC60100)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MC60100  $mC60100
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MC60100 $mC60100)
+    {
+        //
+    }
+}
---
+++ app/Models/MC60100.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MC60100 extends Model
+{
+    //
+}
